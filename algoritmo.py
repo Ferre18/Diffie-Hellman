3fdfc5421bd8615d4a65c83c38892002aaaba518
Adrian <ferrete2003@gmail.com>
2023-11-22 00:51:14 +0100

    el python se ve en el .lex

--- algoritmo.py
+++ algoritmo.py
@@ -1,3 +1,3 @@
 print("Trabajo bien perron. Filantropia en estado puro.")
 for i in range (5) :
-	print("Bendícenos Diffie Hellman, gracias por tus sabios conocimientos")
+	print("Bendicenos Diffie Hellman, gracias por tus sabios conocimientos")
